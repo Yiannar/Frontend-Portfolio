0650d44b1fa3382c9bc9ee9f62551cb13ed2701e
Yiannar <yiannarichey@pursuit.org>
2023-07-16 19:18:07 -0400

    working on info page css in progress for cards

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -3,27 +3,51 @@ import './Info.css'
 
 const Info = () => {
     return (
-        <article>
+        <div className='diamond-info'>
               <h1>Diamond Information</h1>     
+            
+             <div className='card'>
+              <div className='card-body'> 
+              <h6 className='card-title'> What is shape?</h6>  
+              <p className='card-text'> Shape: refers to the outline and physical attributes</p>
+              </div>
+              </div>
 
-              <h6>What is shape?</h6>  
-              <p> Shape: refers to the outline and physical attributes</p>
+              <div className='card'>
+              <div className='card-body'>
+              <h6 className='card-title'> What is cut?</h6> 
+              <p className='card-text'>Cut: refers to the polish, proportions, and symmetry of a diamond, and not in reference to the shape of the diamond</p> 
+              </div>
+              </div>
 
-              <h6>What is cut?</h6> 
-              <p>Cut: refers to the polish, proportions, and symmetry of a diamond, and not in reference to the shape of the diamond</p> 
+              <div className='card'>
+              <div className='card-body'> 
+              <h6 className='card-title'> What is clarity?</h6>  
+              <p className='card-text'>Clarity: a measure of the purity and rarity of the stone, graded by the visibility of these characteristics under 10-power magnification.</p>
+              </div>
+              </div>
 
-              <h6>What is clarity?</h6>  
-              <p>Clarity: a measure of the purity and rarity of the stone, graded by the visibility of these characteristics under 10-power magnification.</p>
+               <div className='card'> 
+              <div className='card-body'>
+              <h6 className='card-title'> What is color?</h6>  
+              <p className='card-text'> Color:  refers to the lack of color in a diamond with the color D meaning its colorless</p>
+              </div>
+              </div>
 
-              <h6>What is color?</h6>  
-              <p> Color:  refers to the lack of color in a diamond with the color D meaning its colorless</p>
+             <div className='card'> 
+              <div className='card-body'> 
+              <h6 className='card-title'> What is carat?</h6>
+              <p className='card-text'>Carat: the unit of measurement for the physical weight of a diamond</p>
+              </div>
+              </div>
 
-              <h6>What is carat?</h6>
-              <p>Carat: the unit of measurement for the physical weight of a diamond</p>
-
-              <h6>What does reported mean?</h6>  
-              <p>Reported: a document or a “diamond certificate”/diamond dossier issued by an independent Gemological Laboratory who authenticates diamonds</p>  
-        </article>
+            <div className='card'> 
+              <div className='card-body'>
+              <h6 className='card-title'> What does reported mean?</h6>  
+              <p className='card-text'>Reported: a document or a “diamond certificate”/diamond dossier issued by an independent Gemological Laboratory who authenticates diamonds</p>
+              </div> 
+              </div>
+        </div>
     );
 };
 
